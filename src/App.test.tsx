f9d21bb2737afff00fae5dac3f135e521aae03a1
Nikola Rashkov <nikola94rashkov@gmail.com>
2024-01-23 17:50:09 +0200

    refactor: 💡 Modify tests
    none

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,7 +4,7 @@ import { render, screen } from '@testing-library/react'
 
 import { App } from './App'
 
-describe('App tests', () => {
+describe('Render the app', () => {
 	it('should contains the heading 1', () => {
 		render(<App />)
 		const heading = screen.getAllByTestId('app-title')
